6cbdfc6be1fef29641de0b827181c08477445641
ENIS <123192012+Enis-ninouka@users.noreply.github.com>
2023-02-07 20:00:42 +0100

    Add files via upload

---
+++ shopping.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const productDetails = [{
+  name: "Airpods Pro",
+  price: 24900,
+  imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJiKtlpQGkIeOyAPV3qQMNkl8uuRzfGWZtIDb_WgDnam8WjhpL&usqp=CAU",
+  qty: 10,
+  heading: "Wireless Noise Cancelling Earphones",
+  des: "AirPods Pro have been designed to deliver active Noise Cancellation for immersive sound. Transparancy mode so much can hear your surroundings."
+}, {
+  name: "Apple Watch",
+  price: 40900,
+  imageUrl: "https://purepng.com/public/uploads/large/apple-watch-pcq.png",
+  qty: 15,
+  heading: "You’ve never seen a watch like this",
+  des: "The most advanced Apple Watch yet, featuring the Always-On Retina display, the ECG app, international emergency calling, fall detection and a built‑in compass."
+}, {
+  name: "Macbook Pro",
+  price: 199900,
+  imageUrl: "https://pngimg.com/uploads/macbook/macbook_PNG8.png",
+  qty: 20,
+  heading: "The best for the brightest",
+  des: "Designed for those who defy limits and change the world, the new MacBook Pro is by far the most powerful notebook we’ve ever made. it’s the ultimate pro notebook for the ultimate user."
+}, {
+  name: "iPhone 11 pro",
+  price: 106600,
+  imageUrl: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-midnight-green-select-2019?
